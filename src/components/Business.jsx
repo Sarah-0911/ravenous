d6aec70286ca75b46f7ff3403ff76e5bf079d3a0
Sarah-0911 <sarah.tabet69@gmail.com>
2023-12-17 20:00:44 +0100

    first component Business in progress

---
+++ src/components/Business.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Business.css';
+
+function Business() {
+  return (
+    <div className='business-container'>
+      <img
+        src="images/pokebowl.jpg"
+        alt="Pokebowl"
+      />
+      <h2>
+        Pokawa
+      </h2>
+      <div className="flex-description">
+        <div className="address">
+          <p>
+            12 Independence Avenue
+          </p>
+          <p>
+            London
+          </p>
+          <p>
+            W12
+          </p>
+        </div>
+        <div>
+          <p className="gold">
+            Hawaii
+          </p>
+          <p className="gold">
+            4.5 stars
+          </p>
+          <p>
+            90 reviews
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Business;
