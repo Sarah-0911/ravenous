e07ecf6c9d42fe8b5f8871206e9ce296eb8d34ab
Sarah-0911 <sarah.tabet69@gmail.com>
2024-01-04 01:01:33 +0100

    add useState to SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SearchBar.module.css';
 
 const SearchBar = () => {
@@ -8,17 +8,53 @@ const SearchBar = () => {
     'Most Reviewed': 'review_count',
   };
 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [location, setLocation] = useState('');
+  const [sortingOption, setsortingOption] = useState(sortingByOptions['Best Match']);
+
+  const handleSearchTermChange = e => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleLocationChange = e => {
+    setLocation(e.target.value);
+  };
+
+  const handleSortingOption = newOption => {
+    setsortingOption(sortingByOptions[newOption]);
+  };
+
   return (
     <div className={styles.searchBar}>
       <div className={styles.filters}>
         {Object.keys(sortingByOptions).map(sortingByOption => {
           let sortingByOptionValue = sortingByOptions[sortingByOption];
-          return <li className={styles.options} key={sortingByOptionValue}>{sortingByOption}</li>
+          return (
+            <li
+              className={styles.options}
+              onClick={() => handleSortingOption(sortingByOption)}
+              key={sortingByOptionValue}
+            >
+              {sortingByOption}
+            </li>
+          );
         })}
       </div>
       <div className={styles.inputs}>
-        <input type="text" className={styles.input} placeholder="Search Businesses" />
-        <input type="text" className={styles.input} placeholder="Where?" />
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Search Businesses"
+          onChange={handleSearchTermChange}
+          value={searchTerm}
+        />
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Where?"
+          onChange={handleLocationChange}
+          value={location}
+          />
       </div>
       <button className={styles.go}>Let's go</button>
     </div>
