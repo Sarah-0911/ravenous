531fd03b07fed4ddc056e296c433587f251eeb2b
Sarah-0911 <sarah.tabet69@gmail.com>
2024-02-22 17:59:36 +0100

    .gitignore is now ok

--- src/utils/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-  apiKey: "ObgRGPf0xwsNLHNksQvyekj7zTvAwbYufJBcTwtOlqp73dMiUhEk1zGC1zSMagdqSCEO4O52XAHwCw8J_e9FpTysxSE19heHO5qMSovEYu7MSwI4A132vTraP3rXZXYx"
-}
-
-export default config;
