f12c29e234e74be918daba9aa53713952647e474
Sarah-0911 <sarah.tabet69@gmail.com>
2024-02-22 17:51:30 +0100

    Prepare for Heroku deployment

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiKey: "P9HT035K6sfI4fx3EDRtcIp0Zxp4B5m5-t6akMvv7jd4T1igkSrk0LuaH1_R2Ikxf-oAagbl33bDFu-AzL8FwSCehArIrFk9sRSKb4QWeNtjlMehQxTs_3Ehb0euZXYx"
+  apiKey: "ObgRGPf0xwsNLHNksQvyekj7zTvAwbYufJBcTwtOlqp73dMiUhEk1zGC1zSMagdqSCEO4O52XAHwCw8J_e9FpTysxSE19heHO5qMSovEYu7MSwI4A132vTraP3rXZXYx"
 }
 
 export default config;
